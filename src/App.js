3e3b17fb44742cd5817107fd270c88bdefb471a5
Nicoogf <nicolasgfalabella@gmail.com>
2023-05-16 14:21:35 -0300

    Configuracion de Paginacion

--- src/App.js
+++ src/App.js
@@ -1,166 +1,35 @@
 import './App.css';
 import PageWrapper from './PageWappers'
 import Pelicula from './pelicula'
+import PeliculaJson from './peliculas.json'
+import Paginacion from './Paginacion'
+import { useState } from 'react';
+
+
+
 
 function App() {
 
+ const [paginaActual, setPaginaActual] = useState(1);
+ const TOTAL_POR_PAGINA = 7 ;
 
- let peliculas = [ 
+ let peliculas = PeliculaJson ;
 
-  
-    {
-    titulo: "oblivion (2012)",
-    calificacion: "8.1",
-    director: "Joss Whedon",
-    actores: "Robert Downey Jr., Chris Evans, Chris Hemsworth",
-    fecha: "1 May 2015",
-    duracion: "2h 21min",
-    img: "images/uploads/mv1.jpg",
-    descripcion: "Earth's mightiest heroes must come together and learn to fight as a team if they are to stop the mischievous Loki and his alien army from enslaving humanity..."
-    },
-    {
-    titulo: "into the wild (2014)",
-    calificacion: "7.5",
-    director: "Joss Whedon",
-    actores: "Robert Downey Jr., Chris Evans, Chris Hemsworth",
-    fecha: "1 May 2015",
-    duracion: "2h 21min",
-    img: "images/uploads/mv2.jpg",
-    descripcion: "Earth's mightiest heroes must come together and learn to fight as a team if they are to stop the mischievous Loki and his alien army from enslaving humanity..."
-    },
-    {
-    titulo: "BLADE RUNNER (2015)",
-    calificacion: "7.3",
-    director: "Peyton Reed",
-    actores: "Paul Rudd, Michael Douglas",
-    fecha: "1 May 2015",
-    duracion: "2h 21min",
-    img: "images/uploads/mv3.jpg",
-    descripcion: "Armed with a super-suit with the astonishing ability to shrink in scale but increase in strength, cat burglar Scott Lang must embrace his inner hero and help..."
-    },
-    {
-    titulo: "MULHOLLAND PRIDE (2013)",
-    calificacion: "7.2",
-    director: "Shane Black",
-    actores: "Robert Downey Jr., Guy Pearce,Don Cheadle",
-    fecha: "1 May 2015",
-    duracion: "2h 21min",
-    img: "images/uploads/mv4.jpg",
-    descripcion: "When Tony Stark's world is torn apart by a formidable terrorist called the Mandarin, he starts an odyssey of rebuilding and retribution."
-    },
-    {
-    titulo: "SKYFALL: EVIL OF BOSS (2013)",
-    calificacion: "7.0",
-    director: "Alan Taylor",
-    actores: "Chris Hemsworth, Natalie Portman,Tom Hiddleston",
-    fecha: "1 May 2015",
-    duracion: "2h 21min",
-    img: "images/uploads/mv5.jpg",
-    descripcion: "When Tony Stark's world is torn apart by a formidable terrorist called the Mandarin, he starts an odyssey of rebuilding and retribution."
-    },
-    {
-    titulo: "oblivion (2012)",
-    calificacion: "8.1",
-    director: "Joss Whedon",
-    actores: "Robert Downey Jr., Chris Evans, Chris Hemsworth",
-    fecha: "1 May 2015",
-    duracion: "2h 21min",
-    img: "images/uploads/mv1.jpg",
-    descripcion: "Earth's mightiest heroes must come together and learn to fight as a team if they are to stop the mischievous Loki and his alien army from enslaving humanity..."
-    },
-    {
-    titulo: "into the wild (2014)",
-    calificacion: "7.5",
-    director: "Joss Whedon",
-    actores: "Robert Downey Jr., Chris Evans, Chris Hemsworth",
-    fecha: "1 May 2015",
-    duracion: "2h 21min",
-    img: "images/uploads/mv2.jpg",
-    descripcion: "Earth's mightiest heroes must come together and learn to fight as a team if they are to stop the mischievous Loki and his alien army from enslaving humanity..."
-    },
-    {
-    titulo: "BLADE RUNNER (2015)",
-    calificacion: "7.3",
-    director: "Peyton Reed",
-    actores: "Paul Rudd, Michael Douglas",
-    fecha: "1 May 2015",
-    duracion: "2h 21min",
-    img: "images/uploads/mv3.jpg",
-    descripcion: "Armed with a super-suit with the astonishing ability to shrink in scale but increase in strength, cat burglar Scott Lang must embrace his inner hero and help..."
-    },
-    {
-    titulo: "MULHOLLAND PRIDE (2013)",
-    calificacion: "7.2",
-    director: "Shane Black",
-    actores: "Robert Downey Jr., Guy Pearce,Don Cheadle",
-    fecha: "1 May 2015",
-    duracion: "2h 21min",
-    img: "images/uploads/mv4.jpg",
-    descripcion: "When Tony Stark's world is torn apart by a formidable terrorist called the Mandarin, he starts an odyssey of rebuilding and retribution."
-    },
-    {
-    titulo: "SKYFALL: EVIL OF BOSS (2013)",
-    calificacion: "7.0",
-    director: "Alan Taylor",
-    actores: "Chris Hemsworth, Natalie Portman,Tom Hiddleston",
-    fecha: "1 May 2015",
-    duracion: "2h 21min",
-    img: "images/uploads/mv5.jpg",
-    descripcion: "When Tony Stark's world is torn apart by a formidable terrorist called the Mandarin, he starts an odyssey of rebuilding and retribution."
-    },
-    {
-    titulo: "oblivion (2012)",
-    calificacion: "8.1",
-    director: "Joss Whedon",
-    actores: "Robert Downey Jr., Chris Evans, Chris Hemsworth",
-    fecha: "1 May 2015",
-    duracion: "2h 21min",
-    img: "images/uploads/mv1.jpg",
-    descripcion: "Earth's mightiest heroes must come together and learn to fight as a team if they are to stop the mischievous Loki and his alien army from enslaving humanity..."
-    },
-    {
-    titulo: "into the wild (2014)",
-    calificacion: "7.5",
-    director: "Joss Whedon",
-    actores: "Robert Downey Jr., Chris Evans, Chris Hemsworth",
-    fecha: "1 May 2015",
-    duracion: "2h 21min",
-    img: "images/uploads/mv2.jpg",
-    descripcion: "Earth's mightiest heroes must come together and learn to fight as a team if they are to stop the mischievous Loki and his alien army from enslaving humanity..."
-    },
-    {
-    titulo: "BLADE RUNNER (2015)",
-    calificacion: "7.3",
-    director: "Peyton Reed",
-    actores: "Paul Rudd, Michael Douglas",
-    fecha: "1 May 2015",
-    duracion: "2h 21min",
-    img: "images/uploads/mv3.jpg",
-    descripcion: "Armed with a super-suit with the astonishing ability to shrink in scale but increase in strength, cat burglar Scott Lang must embrace his inner hero and help..."
-    },
-    {
-    titulo: "MULHOLLAND PRIDE (2013)",
-    calificacion: "7.2",
-    director: "Shane Black",
-    actores: "Robert Downey Jr., Guy Pearce,Don Cheadle",
-    fecha: "1 May 2015",
-    duracion: "2h 21min",
-    img: "images/uploads/mv4.jpg",
-    descripcion: "When Tony Stark's world is torn apart by a formidable terrorist called the Mandarin, he starts an odyssey of rebuilding and retribution."
-    },
-    {
-    titulo: "SKYFALL: EVIL OF BOSS (2013)",
-    calificacion: "7.0",
-    director: "Alan Taylor",
-    actores: "Chris Hemsworth, Natalie Portman,Tom Hiddleston",
-    fecha: "1 May 2015",
-    duracion: "2h 21min",
-    img: "images/uploads/mv5.jpg",
-    descripcion: "When Tony Stark's world is torn apart by a formidable terrorist called the Mandarin, he starts an odyssey of rebuilding and retribution."
-    }
-  ]
+ const cargarPeliculas = () => {
+    peliculas = peliculas.slice( 
+    (paginaActual  - 1) * TOTAL_POR_PAGINA ,
+    paginaActual * TOTAL_POR_PAGINA 
+    );
+ }
 
 
+
+  const getTotalPaginas = ()=>{
+    let cantidadTotalDePeliculas = PeliculaJson.length;
+    return Math.ceil(cantidadTotalDePeliculas / TOTAL_POR_PAGINA)
+  }
+
+  cargarPeliculas();
 
   return (
 
@@ -186,6 +55,9 @@ function App() {
           
         )}
 
+        <Paginacion pagina={paginaActual} total={getTotalPaginas()} onChange={(pagina)=>{
+            setPaginaActual(pagina)
+        }}/>
   
 
    
