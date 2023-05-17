f8c8b913a3a70087dce6f96a74741d7ab55c6234
Nicoogf <nicolasgfalabella@gmail.com>
2023-05-17 08:51:40 -0300

    Falta el fix del BuscarPeliculas()

--- src/App.js
+++ src/App.js
@@ -1,41 +1,63 @@
 import './App.css';
 import PageWrapper from './PageWappers'
 import Pelicula from './pelicula'
-import PeliculaJson from './peliculas.json'
 import Paginacion from './Paginacion'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 
 
 
 function App() {
 
- const [paginaActual, setPaginaActual] = useState(1);
- const TOTAL_POR_PAGINA = 7 ;
+        const [paginaActual, setPaginaActual] = useState(1);
+        const [peliculas, setPeliculas] = useState( [] );
 
- let peliculas = PeliculaJson ;
+        const TOTAL_POR_PAGINA = 7 
 
- const cargarPeliculas = () => {
-    peliculas = peliculas.slice( 
-    (paginaActual  - 1) * TOTAL_POR_PAGINA ,
-    paginaActual * TOTAL_POR_PAGINA 
-    );
- }
+        
 
+        const buscarPeliculas = async() => {
 
+            let url = "https://lucasmoy.dev/data/react/peliculas.json" ;
+            let respuesta = await fetch (url , {
+                  "method" : "GET",
+                  "mode" : "no-cors",
+                  "headers" : {
+                    "Accept" : "application/json",
+                    "Content-Type" : "application/json",
+                    "Origin" : "https://lucasmoy.dev"
+               }
+            })
+           
+            let json = await respuesta.json();
+            setPeliculas(json)
+        }
 
-  const getTotalPaginas = ()=>{
-    let cantidadTotalDePeliculas = PeliculaJson.length;
-    return Math.ceil(cantidadTotalDePeliculas / TOTAL_POR_PAGINA)
-  }
+        const cargarPeliculas = () => {
+              /* 
 
-  cargarPeliculas();
+              peliculas = peliculas.slice( 
+              (paginaActual  - 1) * TOTAL_POR_PAGINA ,
+              paginaActual * TOTAL_POR_PAGINA 
+              ); 
 
+              */
+        }
+
+        const getTotalPaginas = ()=>{
+          let cantidadTotalDePeliculas = peliculas.length;
+          return Math.ceil(cantidadTotalDePeliculas / TOTAL_POR_PAGINA)
+        }
+
+       cargarPeliculas()
+     
   return (
 
     <div>
 
       <PageWrapper>
+
+       
 
         { peliculas.map (pelicula => 
          
